20500c10711df516a6d4ab1d99227bb20ee973be
matias300nes <matiendres@gmail.com>
2021-08-30 15:32:22 -0300

    botones terminados

--- v1/app.js
+++ v1/app.js
@@ -1,12 +1,14 @@
 function add(){
     InputValue = document.getElementById("textinput")
     checkbox = `<input class="checkmark" type="checkbox" onClick="this.parentElement.classList.toggle('checked')">` //done logic
-    deletebutton = `<button class="btn" onClick="this.parentElement.remove()"><i class="far fa-trash-alt"></i></button>` //delete logic
+    deletebutton = `<button class="btn delete" onClick="this.closest('li').remove()"><i class="far fa-trash-alt"></i></button>` //delete logic
+    sharebutton = `<button class="btn primary" onClick="share(this.closest('li'))"><i class="far fa-share-square"></i></button>` //delete logic
+    copybutton = `<button class="btn primary" onClick="copy(this.closest('li'))"><i class="far fa-copy"></i></button>` //delete logic
     var Li = document.createElement("li")
     Li.setAttribute("class","list-item")
-    Li.innerHTML = `<div class="check-and-text">${checkbox + InputValue.value}</div>` + deletebutton
+    Li.innerHTML = `<div class="check-and-text">${checkbox} <p class='text'>${InputValue.value}</p></div> <div>${copybutton + sharebutton + deletebutton}</div>`
     var mylist = document.getElementById("mylist")
-    mylist.appendChild(Li)
+    mylist.prepend(Li)
     InputValue.value = null
 }
 
@@ -18,4 +20,22 @@ function toggleFullscreen(button) {
         document.exitFullscreen();
         button.innerHTML = '<i class="fas fa-expand fa-lg"></i>';
     }
+}
+
+function share(Li){
+    const taskContent = Li.getElementsByClassName("text")[0].textContent
+    navigator.share({
+        title: "Tarea compartida desde mi app:",
+        text: taskContent,
+        url: document.URL
+    }).then(
+        () => console.log("tarea compartida...")
+    ).catch(
+        () => window.alert("ups, algo salio mal ❗")
+    )
+}
+
+function copy(Li){
+    const taskContent = Li.getElementsByClassName("text")[0].textContent
+    navigator.clipboard.writeText(taskContent).then(() => window.alert("tarea copiada 📋"))
 }
